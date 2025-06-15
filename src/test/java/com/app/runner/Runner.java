39796ee59a97da686c73aec8c92fd4dac6eda44d
YUVARAJSEKAR12 <61101411+YUVARAJSEKAR12@users.noreply.github.com>
2025-06-15 20:10:50 +0530

    Update Runner.java

--- src/test/java/com/app/runner/Runner.java
+++ src/test/java/com/app/runner/Runner.java
@@ -1,22 +1,18 @@
-package com.app.runner;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\HP\\eclipse-workspace"
-		+ "\\Develop\\new\\Oct_Cucumber\\src\\test\\java\\com\\app\\feature", 
-		glue = "com.app.stepdefinition",
-		dryRun = false,
-		tags = "@Smoke",
-		publish = true,
-		monochrome = true,
-		plugin = {"pretty","html:target/cucumber-reports.html",
-				"json:target/cucumber.json",
-				"rerun:target/rerun.txt"}
-		)
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {"stepdefinitions"},
+    plugin = {
+        "pretty",
+        "json:target/cucumber.json", 
+        "html:target/cucumber-reports.html"
+    },
+    monochrome = true
+)
 public class Runner {
-
 }
